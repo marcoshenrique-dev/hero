2a5d4a986a250d269230e8f2d397066bba0517e5
Marcos Henrique <marcoshenriqueh393@gmail.com>
2020-11-23 17:41:00 -0300

    feat: add class AppError in the server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import 'express-async-errors';
 
 import './database';
+import AppError from './errors/AppError';
 import routes from './routes';
 
 const app = express();
@@ -14,6 +15,22 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+   if (err instanceof AppError) {
+      response.status(err.statusCode).json({
+         status: 'error',
+         message: err.message,
+      });
+   }
+
+   console.error(err);
+
+   return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+   });
+});
 
 app.listen(3333, () =>
    console.log('😀 server started at http://localhost:3333'),
