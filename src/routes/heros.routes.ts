21ad04e49e06a35d8437650ac552d003848fc039
Marcos Henrique <marcoshenriqueh393@gmail.com>
2020-11-23 17:13:17 -0300

    feat: add ServiceFindHero in routes

--- src/routes/heros.routes.ts
+++ src/routes/heros.routes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 import CreateHeroService from '../services/CreateHeroService';
+import FindHeroService from '../services/FindHeroService';
 
 const herosRoutes = Router();
 
-herosRoutes.get('/', async (request, response) => {});
+herosRoutes.get('/:hero', async (request, response) => {
+   const { hero } = request.params;
+
+   const findHeroService = new FindHeroService();
+
+   const heroResponse = await findHeroService.execute({
+      name: hero,
+   });
+
+   return response.json(heroResponse);
+});
 
 herosRoutes.post('/', async (request, response) => {
    const { name, author, description, image_url } = request.body;
